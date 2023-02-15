c5579d30e7ba3089b7a3162791d6bc94885e232a
Ryan SVIHLA <105286284+rsvihladremio@users.noreply.github.com>
2023-02-15 10:54:50 +0100

    fixing license headers

--- src/glog/pauses.rs
+++ src/glog/pauses.rs
@@ -1,4 +1,3 @@
-use std::fmt;
 // Copyright 2022 Dremio
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +11,7 @@ use std::fmt;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use std::fmt;
 use std::str::FromStr;
 use std::{collections::HashMap, error::Error};
 
